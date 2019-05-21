3222f3e3629119d6d3394c5c65bc192e40991464
João Pedro Dias <joaopmgd@gmail.com>
2019-05-21 17:17:40 -0300

    Create .go

---
+++ .go
@@ -0,0 +1,15 @@
+go get github.com/pkg/errors
+go get gopkg.in/yaml.v2
+go get github.com/spf13/afero
+go get github.com/sirupsen/logrus
+go get github.com/davecgh/go-spew/spew
+go get -u -v github.com/aws/aws-lambda-go/events
+go get github.com/gin-gonic/gin
+go get github.com/swaggo/gin-swagger
+go get github.com/swaggo/gin-swagger/swaggerFiles
+go get gopkg.in/gormigrate.v1
+go get github.com/joho/godotenv/autoload
+go get github.com/alecthomas/template
+go get github.com/lib/pq
+go get github.com/satori/go.uuid
+go get github.com/segmentio/kafka-go
